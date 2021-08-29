26d3fa76d348fc818ed3d0be58cb43d77f80dddb
Lorenzo Cella <lorenz.cella@gmail.com>
2021-08-29 23:04:58 +0200

    added auto download and google ad conversion tag

--- src/pages/Success.vue
+++ src/pages/Success.vue
@@ -1,26 +1,43 @@
 <template>
   <Layout>
     <div class="pt-8 md:pt-16">
-      <h1 class="text-center font-bold text-4xl text-ui-primary">Thanks for you email</h1>
+      <h1 class="text-center font-bold text-4xl text-ui-primary">
+        Thanks for you email
+      </h1>
     </div>
     <div class="flex-col text-center">
-      <Logo :width="200"/>
+      <Logo :width="200" />
       <p>You can download the app from the link below.</p>
       <p>I would like to have your feedback at info@availableonteams.com</p>
-      
-      <a class="text-xl underline" href="/AvailableOnTeamsDEMO.exe" download>Download DEMO</a>
 
+      <a class="text-xl underline" href="/AvailableOnTeamsDEMO.exe" download
+        >Download DEMO</a
+      >
     </div>
     <div class="mt-10 flex items-center justify-center">
-      <g-link to="/" class="font-bold border-b border-ui-primary text-xl">Go back</g-link>.
+      <g-link to="/" class="font-bold border-b border-ui-primary text-xl"
+        >Go back</g-link
+      >.
     </div>
   </Layout>
 </template>
 
 <script>
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "AW-995543636");
+
+setTimeout(function () {
+  window.location = "/AvailableOnTeamsDEMO.exe";
+}, 3000);
+
 export default {
   metaInfo: {
-    title: 'Success'
-  }
-}
+    title: "Success",
+  },
+};
 </script>
